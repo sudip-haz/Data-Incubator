9366fb7dc3dee6ec4eba252330e918e96f3405fa
sudip-haz <32990061+sudip-haz@users.noreply.github.com>
2018-05-01 03:51:51 -0500

    Add files via upload

---
+++ C3_plot2.m
@@ -0,0 +1,4 @@
+robot = imread('robot prices.png');
+imshow(robot);
+
+% link to document source:  https://www.mckinsey.com/business-functions/operations/our-insights/automation-robotics-and-the-factory-of-the-future 
